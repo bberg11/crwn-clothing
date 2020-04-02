588243f8afecb8e0eae1f9afa9aa9e47f02d8952
Brian Berg <bberg11@gmail.com>
2020-04-02 00:01:24 -0400

    109. Add To Cart Styling

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component';
+
 import './collection-item.styles.scss';
 
 const CollectionItem = (props) => (
@@ -14,6 +16,9 @@ const CollectionItem = (props) => (
       <span className="name">{props.item.name}</span>
       <span className="price">{props.item.price}</span>
     </div>
+    <CustomButton inverted>
+        Add to cart
+      </CustomButton>
   </div>
 );
 
