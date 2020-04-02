9d2b0a4bdd6620445476d756f6da602a62371bd5
Brian Berg <bberg11@gmail.com>
2020-04-02 15:01:19 -0400

    119. Checkout Page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { selectCurrentUser } from './redux/user/user.selectors';
 import Homepage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Checkout from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
@@ -57,6 +58,7 @@ class App extends React.Component {
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={Shop} />
           <Route exact path='/sign-in' render={this.signInRenderValue} />
+          <Route exact path='/checkout' component={Checkout} />
         </Switch>
       </div>
     );
---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './checkout.styles.scss';
+
+const Checkout = () => (
+  <div>Checkout Page</div>
+);
+
+export default Checkout;
