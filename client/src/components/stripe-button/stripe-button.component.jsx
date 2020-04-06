bdf0bce25dda3f766b81538cb010708df6234a4d
Brian Berg <bberg11@gmail.com>
2020-04-05 23:39:39 -0400

    212. Connecting Client To Server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,7 +8,19 @@ const StripeButton = ({ price }) => {
 
   const onToken = (token) => {
     console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: '/payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then((response) => {
+      alert('Payment successful');
+    }).catch((error) => {
+      console.log('Payment error: ' + JSON.parse(error));
+      alert('There was an error with your payment');
+    });
   };
 
   return (
