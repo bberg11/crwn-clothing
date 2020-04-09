05e357ad2740ec5a11c801ad9da7f2d63ccce6ef
Brian Berg <bberg11@gmail.com>
2020-04-09 17:25:11 -0400

    Show cart when adding item to cart and auto hide after 5 seconds

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { addItem } from '../../redux/cart/cart.actions';
+import { addItem, toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import Button from '../button/button.component';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ item, addItem }) => (
+const handleAddItemClick = (addItem, item, toggleCartHidden) => {
+  addItem(item);
+  toggleCartHidden();
+
+  setTimeout(() => {
+    toggleCartHidden();
+  }, 5000);
+};
+
+const CollectionItem = ({ item, addItem, toggleCartHidden }) => (
   <div className="collection-item">
     <div
       className="collection-item__image"
@@ -21,7 +30,7 @@ const CollectionItem = ({ item, addItem }) => (
     </div>
     <div className="collection-item__add-to-cart">
       <Button
-        onClick={() => addItem(item)}
+        onClick={() => handleAddItemClick(addItem, item, toggleCartHidden)}
         className="button button--full-width button--inverted"
       >
         Add to cart
@@ -32,6 +41,7 @@ const CollectionItem = ({ item, addItem }) => (
 
 const mapDispatchToProps = (dispatch) => ({
   addItem: (item) => dispatch(addItem(item)),
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
