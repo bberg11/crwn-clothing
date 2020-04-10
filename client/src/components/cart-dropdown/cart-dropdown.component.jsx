051c0e2155fcebd5de8a0d2fed1f07b898425402
Brian Berg <bberg11@gmail.com>
2020-04-09 21:57:16 -0400

    Toggle CartDropdown hidden when clicking outside element

--- client/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -11,30 +11,60 @@ import Button from '../button/button.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history, dispatch }) => (
-  <div className="cart-dropdown">
-    <div className="cart-dropdown__items">
-      {cartItems.length ? (
-        cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))
-      ) : (
-        <span className="cart-dropdown__empty">Your cart is empty</span>
-      )}
-    </div>
-    <div className="cart-dropdown__action">
-      <Button
-        className="button button--full-width"
-        onClick={() => {
-          history.push('/checkout');
-          dispatch(toggleCartHidden());
-        }}
-      >
-        GO TO CHECKOUT
-      </Button>
-    </div>
-  </div>
-);
+class CartDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.cartDropdownRef = React.createRef();
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  handleClickOutside = (event) => {
+    const { dispatch } = this.props;
+    const dropdown = this.cartDropdownRef.current;
+    const clickedElement = event.target;
+
+    if (!dropdown.contains(clickedElement)) {
+      dispatch(toggleCartHidden());
+    }
+  };
+
+  render() {
+    const { cartItems, history, dispatch } = this.props;
+
+    return (
+      <div className="cart-dropdown" ref={this.cartDropdownRef}>
+        <div className="cart-dropdown__items">
+          {cartItems.length ? (
+            cartItems.map((cartItem) => (
+              <CartItem key={cartItem.id} item={cartItem} />
+            ))
+          ) : (
+            <span className="cart-dropdown__empty">Your cart is empty</span>
+          )}
+        </div>
+        <div className="cart-dropdown__action">
+          <Button
+            className="button button--full-width"
+            onClick={() => {
+              history.push('/checkout');
+              dispatch(toggleCartHidden());
+            }}
+          >
+            GO TO CHECKOUT
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
