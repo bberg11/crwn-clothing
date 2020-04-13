37389ae6c3cbe62e68b7d66db2060493213e58fa
Brian Berg <bberg11@gmail.com>
2020-04-13 12:24:02 -0400

    Begin implementing tests by refactoring homepage directory

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import { selectDirectorySections } from '../../redux/directory/directory.selectors';
-
-import DirectoryItem from '../../components/directory-item/directory-item.component';
-
-import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
 
 const Homepage = ({ sections }) => (
   <section className="homepage">
-    <div className="homepage__directory">
-      {sections.map((section) => (
-        <DirectoryItem key={section.id} section={section} />
-      ))}
-    </div>
+    <Directory />
   </section>
 );
 
-const mapStateToProps = createStructuredSelector({
-  sections: selectDirectorySections,
-});
-
-export default connect(mapStateToProps)(Homepage);
+export default Homepage;
